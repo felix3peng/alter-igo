0e18537899a4bb5671addff66dccaa4d958d4a00
felix3peng <felix.peng@zs.com>
2022-07-27 14:59:04 -0500

    Added descriptive comments to scripts.js // trim code and command when editing (whitespace created)

--- alter_igo/home/static/scripts.js
+++ alter_igo/home/static/scripts.js
@@ -1,13 +1,23 @@
+/*
+SCRIPT FUNCTIONS FOR APP ROUTES
+*/
+
+// setting up 3 variables; i to count number of entries, nt for number of tables, np for number of plots
 var i=1;
 var nt=0;
 var np=0;
 
+// function to submit command entered in text box to backend for processing
+// receives the outputs from backend and displays them in the appropriate manner
 $('#submit').click(function(){
     $.getJSON("/home/process", {
+        // send the contents of the text box
         command: $('#cmd').val(),
     }, function(data){
+        // generate command, code, and output divs
         var wrapper_id = "wrapper" + data.outputs[4];
         $('.content').append('<div id="' + wrapper_id + '" class="wrapper"></div>');
+        // in the command div, display the number of the entry, the command, and the button toolbar
         $('#' + wrapper_id).append('<div class="commandwrapper">\
                                         <div class="numlabel">['+i+']: </div>\
                                         <div class="command_disp" id="command'+i+'"><b>'+data.outputs[1]+'</b></div>\
@@ -27,6 +37,8 @@ $('#submit').click(function(){
                                         </div>\
                                     </div>\
                                 </div>');
+        // display the code that was generated, with visibility set based on code checkbox and presence of output
+        // if showcode checkbox is checked, display code
         if (document.getElementById('checkbox').checked) {
             $('#' + wrapper_id).append("<div class='codewrapper'>\
                                             <button data-toggle='collapse' class='cobutton' data-target='#code"+i+"'></button>\
@@ -34,6 +46,7 @@ $('#submit').click(function(){
                                                 <pre><code class='language-python'>"+data.outputs[2]+"</code></pre>\
                                             </div>\
                                         </div>");
+        // otherwise, if the output is empty, show the code
         } else if (data.outputs[3] == "") {
             $('#' + wrapper_id).append("<div class='codewrapper'>\
                                             <button data-toggle='collapse' class='cobutton' data-target='#code"+i+"'></button>\
@@ -41,6 +54,7 @@ $('#submit').click(function(){
                                                 <pre><code class='language-python'>"+data.outputs[2]+"</code></pre>\
                                             </div>\
                                         </div>");
+        // otherwise, if the output is not empty, hide the code div
         } else {
             $('#' + wrapper_id).append("<div class='codewrapper'>\
                                             <button data-toggle='collapse' class='cobutton collapsed' data-target='#code"+i+"'></button>\
@@ -49,27 +63,33 @@ $('#submit').click(function(){
                                             </div>\
                                         </div>");
         }
+        // display the output, with type based on outputtype and visibility based on presence of output
         let outputdiv = document.createElement('div');
         outputdiv.classList = "output";
+        // if the outputtype is image, display as such and include a link to download the image. Increment np.
         if (data.outputs[0] == 'image') {
             $(outputdiv).append(data.outputs[3]);
             $('#' + wrapper_id).append(outputdiv);
             var imgsrc = document.getElementById('image'+np).src;
             $(outputdiv).append("<div class='dl_btn'><a href="+imgsrc+" download='image"+np+"'>Download as PNG</a></div>");
             np++;
+        // if the outputtype is dataframe, display as a table and include a link to download the table. Increment nt.
         } else if (data.outputs[0] == 'dataframe') {
             $(outputdiv).append(data.outputs[3]);
             $('#' + wrapper_id).append(outputdiv);
             $(outputdiv).append("<div class='dl_btn'><a href='#' onclick='download_table_as_csv("+'table'+nt+");'>Download as CSV</a></div>");
             nt++;
+        // otherwise, as long as the output is not empty, display as text
         } else if (data.outputs[3] !== "") {
             $(outputdiv).append('<pre><code class="language-plaintext">'+data.outputs[3]+'</code></pre>');
             $('#' + wrapper_id).append(outputdiv);
+        // if output is empty, make the output div invisible
         } else {
             $(outputdiv).append('<p>No output (I\'m hidden by default)</p>');
             $('#' + wrapper_id).append(outputdiv);
             $(outputdiv).hide();
         }
+        // if the codeblock was empty, throw an error alert
         if (data.outputs[2] == '') {
             alert("Sorry, I couldn't understand your request.");
         }
@@ -81,6 +101,7 @@ $('#submit').click(function(){
 });
 });
 
+// on clicking the clear button, clear all content and reset the local session storage
 $('#clear').click(function(){
     i=1;
     nt=0;
@@ -94,6 +115,7 @@ $('#clear').click(function(){
     });
 });
 
+// modify checkbox activity
 $('#showcode').change(function () {
     if ($(this).is(":checked")) {
         $('#table').show();
@@ -102,6 +124,7 @@ $('#showcode').change(function () {
     }
 });
 
+// Ctrl+Enter will submit code
 $('#cmd').keydown(function(e){
     if(e.ctrlKey && (e.keyCode == 13 || e.keyCode == 10)) {
         e.preventDefault();
@@ -110,6 +133,7 @@ $('#cmd').keydown(function(e){
     }
 });
 
+// shrink or expand sidebar on click
 var sideBar = document.getElementById('entry');
 var arrBtn = document.getElementById('open-close-btn');
 var arrIco = document.getElementById('arrowIcon');
@@ -132,14 +156,14 @@ $('#open-close-btn').click(function() {
     };
 });
 
-
+// show a modal when the user submits a command
 $body = $("body");
 $(document).on({
     ajaxStart: function() { $body.addClass("loading");    },
     ajaxStop: function() { $body.removeClass("loading"); }    
 });
 
-
+// link to download table as a csv
 function download_table_as_csv(table_id) {
     var rows = document.querySelectorAll('#'+table_id.id+' tr');
     var csv = [];
@@ -166,6 +190,7 @@ function download_table_as_csv(table_id) {
     document.body.removeChild(link);
 }
 
+// function to pass positive feedback (or cancel positive feedback) to the db
 function positive(button_id) {
     $.getJSON('/home/positive_feedback', {
             db_id: button_id,
@@ -181,6 +206,7 @@ function positive(button_id) {
         });
 }
 
+// function to pass negative feedback (or cancel negative feedback) to the db
 function negative(button_id) {
     $.getJSON('/home/negative_feedback', {
             db_id: button_id,
@@ -196,6 +222,7 @@ function negative(button_id) {
         });
 }
 
+// function to trigger editable command and codeblock
 function editcode(code_div_id) {
     var targetdiv = document.getElementById('code'+code_div_id);
     var commanddiv = document.getElementById('command'+code_div_id);
@@ -222,13 +249,13 @@ function editcode(code_div_id) {
     editableCmd.id = "editcmd" + code_div_id;
     editableCmd.style.height = h2;
     editableCmd.style.width = "calc(100% - 23rem)";
-    editableCmd.value = origcommand;
+    editableCmd.value = origcommand.trim();
 
     editableText.classList = "editbox";
     editableText.id = "editcode"+code_div_id;
     editableText.style.height = h;
     editableText.style.width = "100%";
-    editableText.value = origcode;
+    editableText.value = origcode.trim();
 
     confirmButton.classList = "confirm";
     confirmButton.id = "confirm"+code_div_id;
@@ -288,6 +315,7 @@ function editcode(code_div_id) {
     });
 }
 
+// function to pass edited content to backend for processing
 function editconfirm(code_div_id) {
     var editbox = document.getElementById("editcode"+code_div_id);
     var editcmd = document.getElementById("editcmd"+code_div_id);
@@ -305,7 +333,7 @@ function editconfirm(code_div_id) {
     newcmd.classList = "command_disp";
     newcmd.id = "command"+code_div_id;
     newcmd.appendChild(document.createElement("b"));
-    newcmd.lastChild.innerText = editedcmd;
+    newcmd.lastChild.innerText = editedcmd.trim();
     
     targetdiv.remove();
     newdiv.classList = "collapse in";
@@ -313,7 +341,7 @@ function editconfirm(code_div_id) {
     newdiv.appendChild(document.createElement("pre"));
     newdiv.lastChild.appendChild(document.createElement("code"));
     newdiv.lastChild.lastChild.class = "language-python";
-    newdiv.lastChild.lastChild.textContent = editedcode;
+    newdiv.lastChild.lastChild.textContent = editedcode.trim();
 
     editButton.classList = "edit";
     editButton.id = "edit"+code_div_id;
@@ -363,11 +391,10 @@ function editconfirm(code_div_id) {
     })
 }
 
+// function to cancel the edit and restore original status
 function editcancel(code_div_id) {
     var targetdiv = document.getElementById('code'+code_div_id);
     var commanddiv = document.getElementById('command'+code_div_id);
-    var origcode = targetdiv.textContent;
-    var origcmd = commanddiv.innerText;
     var editbox = document.getElementById("editcode"+code_div_id);
     var editcmd = document.getElementById("editcmd"+code_div_id);
     var confirmButton = document.getElementById('confirm'+code_div_id);
@@ -392,6 +419,7 @@ function editcancel(code_div_id) {
     hljs.highlightAll();
 }
 
+// function to delete a code entry
 function deleteentry(record_id) {
     var wrapper = document.getElementById('wrapper'+record_id);
     var linebreak = wrapper.nextSibling;
@@ -403,6 +431,7 @@ function deleteentry(record_id) {
     })
 }
 
+// tab behavior in textareas
 $('textarea').keydown(function(e) {
     if(e.keyCode == 9) {
         var start = this.selectionStart;
